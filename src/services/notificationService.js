3de4dce20986ceb01f456fd5297dcf9085404f63
Bullzai <hardas322@yahoo.com>
2023-08-15 02:11:24 +0100

    fix notifications for 2x sensors

--- src/services/notificationService.js
+++ src/services/notificationService.js
@@ -8,7 +8,7 @@ if (result.error) {
   process.exit(1);
 }
 
-export function sendEmail(temperature) {
+export function sendEmail(temperatureData) {
   let transporter = nodemailer.createTransport({
     port: process.env.EMAIL_PORT,
     host: process.env.EMAIL_IP,
@@ -26,7 +26,7 @@ export function sendEmail(temperature) {
     from: process.env.EMAIL_FROM,
     to: process.env.EMAIL_TO,
     subject: process.env.EMAIL_SUBJECT,
-    text: `The temperature has reached ${temperature}°C`,
+    text: `${process.env.EMAIL_MESSAGE} Sensor 1 temperature: ${temperatureData.sensor1_temperature}°C, Sensor 2 temperature: ${temperatureData.sensor2_temperature}°C`,
   };
 
   transporter.sendMail(mailOptions, function (error, info) {
@@ -38,10 +38,10 @@ export function sendEmail(temperature) {
   });
 }
 
-export function sendSlackMessage(temperature) {
+export function sendSlackMessage(temperatureData) {
   axios
     .post(process.env.SLACK_WEB_HOOK, {
-      text: `${process.env.SLACK_MESSAGE} ${temperature}°C`,
+      text: `${process.env.SLACK_MESSAGE} Sensor 1 temperature: ${temperatureData.sensor1_temperature}°C, Sensor 2 temperature: ${temperatureData.sensor2_temperature}°C`,
     })
     .then((response) => {
       console.log("Message sent to Slack");
@@ -51,11 +51,14 @@ export function sendSlackMessage(temperature) {
     });
 }
 
-export function sendNotifications(temperatureValue) {
+export function sendNotifications(temperatureData) {
   const threshold = 25; // set the value in C
 
-  if (temperatureValue > threshold) {
-    sendEmail(temperatureValue);
-    sendSlackMessage(temperatureValue);
+  if (
+    temperatureData.sensor1_temperature > threshold ||
+    temperatureData.sensor2_temperature > threshold
+  ) {
+    sendEmail(temperatureData);
+    sendSlackMessage(temperatureData);
   }
 }
