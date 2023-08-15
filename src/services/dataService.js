1a446d196b1cf222ba56d22b70a890f92205db86
Bullzai <hardas322@yahoo.com>
2023-08-15 01:38:43 +0100

    Adjustments for 2x sensors and their readings

--- src/services/dataService.js
+++ src/services/dataService.js
@@ -1,29 +1,40 @@
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
 function formatDate(isoString) {
   const date = new Date(isoString);
 
   // Convert to Dublin time
-  const options = { timeZone: 'Europe/Dublin', year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', hour12: false };
-  const formatted = new Intl.DateTimeFormat('en-US', options).format(date);
+  const options = {
+    timeZone: "Europe/Dublin",
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false,
+  };
+  const formatted = new Intl.DateTimeFormat("en-US", options).format(date);
 
-  const [_, month, day, year, hour, minute] = formatted.match(/(\d{2})\/(\d{2})\/(\d{4}), (\d{2}):(\d{2})/);
+  const [_, month, day, year, hour, minute] = formatted.match(
+    /(\d{2})\/(\d{2})\/(\d{4}), (\d{2}):(\d{2})/
+  );
 
   return `${month}/${day}/${year} - ${hour}:${minute}`;
 }
 
-export function updateReadingsFile(temperatureValue) {
+export function updateReadingsFile(temperatureData) {
   const currentDir = path.dirname(fileURLToPath(import.meta.url));
-  const readingsPath = path.join(currentDir, '../..', 'readings.json');
-  const data = fs.readFileSync(readingsPath, 'utf-8');
+  const readingsPath = path.join(currentDir, "../..", "readings.json");
+  const data = fs.readFileSync(readingsPath, "utf-8");
   const readings = JSON.parse(data);
   const timestamp = new Date().toISOString();
 
   const newReading = {
     timestamp: formatDate(timestamp),
-    temperature: temperatureValue
+    sensor1_temperature: temperatureData.sensor1_temperature,
+    sensor2_temperature: temperatureData.sensor2_temperature,
   };
 
   readings.push(newReading);
--- src/services/sensorService.js
+++ src/services/sensorService.js
@@ -8,12 +8,23 @@ if (result.error) {
 }
 
 export function readTemp() {
-  const SENSOR_PATH = process.env.SENSOR_PATH;
-  const data = fs.readFileSync(SENSOR_PATH, "utf8");
-  console.log(data);
-  const match = data.match(/t=(\d+)/);
-  if (match) {
-    return parseFloat(match[1]) / 1000.0;
-  }
-  return null;
+  const SENSOR_PATH1 = process.env.SENSOR_PATH1;
+  const SENSOR_PATH2 = process.env.SENSOR_PATH2;
+
+  // read temp from Sensor-1
+  const data1 = fs.readFileSync(SENSOR_PATH1, "utf8");
+  console.log(`Sensor 1 Data: ${data1}`);
+  const match1 = data1.match(/t=(\d+)/);
+  const temp1 = match1 ? parseFloat(match1[1]) / 1000.0 : null;
+
+  // read temp from Sensor-2
+  const data2 = fs.readFileSync(SENSOR_PATH2, "utf8");
+  console.log(`Sensor 2 Data: ${data2}`);
+  const match2 = data2.match(/t=(\d+)/);
+  const temp2 = match2 ? parseFloat(match2[1]) / 1000.0 : null;
+
+  return {
+    sensor1_temperature: temp1,
+    sensor2_temperature: temp2,
+  };
 }
